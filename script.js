6d9d1b602936146298641bf221b5fbf8d90d0b9d
Higor Farias <higornf2009@gmail.com>
2023-06-15 21:36:26 -0300

    finalizando javascript e projeto

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 function toggleMode() {
     const html = document.documentElement
-
+    /* jeito simples */
+    html.classList.toggle("light")
 //    if(html.classList.contains('light')) {
 //        html.classList.remove("light")
 //    } else {
 //        html.classList.add("light")
 //    }
 
-    /* jeito simples */
-    html.classList.toggle('light')
+    // Alterar a img ao clicar no botão
+const img = document.querySelector("#profile img")
+
+    if(html.classList.contains("light")) {
+        //Se tiver light mode, adicionar a imagem light
+        img.setAttribute("src", "./assets/me-light.jfif")
+    } else {
+        // Se tiver sem light mode, manter a imagem normal
+        img.setAttribute("src", "./assets/avatarme.jfif")
+    }
 
 }
